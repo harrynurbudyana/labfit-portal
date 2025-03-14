1eec3b68574e38172278e0445349b66bac12b9f6
harrynurbudyana <harrynurbudyana@gmail.com>
2025-03-14 11:10:37 +0700

    update color scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,24 +33,24 @@ export default function Home() {
     };
 
   return (
-    <div className="items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-base-200">
+    <div className="bg-black items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 lg:items-center sm:items-center">
         <div className="grid justify-items-center">
         <Image src={logolab} width={400} height={400} alt="" className=""/> 
         </div>
         <div className="text-center">
-        <div className="text-2xl justify-center sm:justify-items-center">Selamat datang di Portal Laboratorium Fakultas Ilmu Terapan</div>
+        <div className="text-white text-2xl justify-center sm:justify-items-center">Selamat datang di Portal Laboratorium Fakultas Ilmu Terapan</div>
         </div>
         <div>
         <div className="flex flex-col lg:flex-row gap-8 items-center">
-          <div className="card card-compact bg-base-100 w-96 shadow-xl">
+          <div className="bg-slate-600 text-white card card-compact bg-base-100 w-96 shadow-xl">
             <figure className="image-full">
               <Image
                 src={simlab}
                 alt="simlab" />
             </figure>
-            <div className="card-body">
-              <h2 className="card-title">Simlabfit</h2>
+            <div className="text-white card-body">
+              <h2 className="text-white card-title">Simlabfit</h2>
               <p>Sistem Manajemen Laboratorium Fakultas Ilmu Terapan</p>
               <div className="card-actions justify-end">
                 <Link href="https://sim.labfit.id" target="_blank">
@@ -59,7 +59,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="card card-compact bg-base-100 w-96 shadow-xl">
+          <div className="text-white bg-slate-600 card card-compact w-96 shadow-xl">
             <figure className="image-full">
               <Image
                 width={400}
@@ -67,7 +67,7 @@ export default function Home() {
                 src={websiteicon}
                 alt="Website" />
             </figure>
-            <div className="card-body">
+            <div className="card-body border-radius-lg">
               <h2 className="card-title">Website Laboratorium FIT</h2>
               <p>Halaman Website Laboratorium FIT yang menyediakan informasi seputar laboratorium FIT.</p>
               <div className="card-actions justify-end">
@@ -77,7 +77,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="card card-compact bg-base-100 w-96 shadow-xl">
+          <div className="text-white bg-slate-600 card card-compact bg-base-100 w-96 shadow-xl">
             {/* <figure className="image-full">
               <Image
                 src={comingsoon}
@@ -105,7 +105,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="mt-10">
-        <div className="flex items-center">Silakan Hubungi Kami!</div>
+        <div className="text-white flex items-center">Silakan Hubungi Kami!</div>
         <div className="mt-5 grid grid-flow-col justify-items-center gap-3">
           <SocialIcon url="https://www.instagram.com/lab.fit.telu" target="_blank"></SocialIcon>
           <SocialIcon network="whatsapp" url="https://wa.me/6285172088181" target="_blank"></SocialIcon>
